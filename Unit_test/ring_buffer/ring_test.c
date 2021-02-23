956228bdd3b3ae50cae1de395714b1942998469f
magiczny-kacper <kapibrv97@gmail.com>
2021-02-23 21:22:14 +0100

    Updated unit tests.

--- Unit_test/ring_buffer/ring_test.c
+++ Unit_test/ring_buffer/ring_test.c
@@ -25,7 +25,7 @@ TEST(ring_tests, init)
    RingBuffer_t myRing;
    uint8_t buffer [256];
 
-   RingInit(&myRing, &buffer[0], 256);
+   RingInit(&myRing, &buffer[0], 256, sizeof(uint8_t));
 
    TEST_ASSERT_EQUAL(256, myRing.size);
    TEST_ASSERT_POINTERS_EQUAL(&buffer, myRing.buffer);
@@ -35,15 +35,15 @@ TEST(ring_tests, init_with_null)
 {
    RingBuffer_t myRing;
    uint8_t buf[256];
-   TEST_ASSERT_EQUAL(NO_PTR, RingInit(&myRing, NULL, 256));
-   TEST_ASSERT_EQUAL(NO_PTR, RingInit(NULL, &buf[0], 256));
+   TEST_ASSERT_EQUAL(NO_PTR, RingInit(&myRing, NULL, 256, sizeof(uint8_t)));
+   TEST_ASSERT_EQUAL(NO_PTR, RingInit(NULL, &buf[0], 256, sizeof(uint8_t)));
 }
 
 TEST(ring_tests, init_with_no_size)
 {
    RingBuffer_t myRing;
    uint8_t arr[10];
-   TEST_ASSERT_EQUAL(NO_DATA, RingInit(&myRing, &arr[0], 0));
+   TEST_ASSERT_EQUAL(NO_DATA, RingInit(&myRing, &arr[0], 0, sizeof(uint8_t)));
 }
 
 TEST(ring_tests, write_byte)
@@ -51,16 +51,16 @@ TEST(ring_tests, write_byte)
    RingBuffer_t myRing;
    uint8_t arr[10];
    uint8_t testValue = 10;
-   RingInit(&myRing, &arr[0], 10);
+   RingInit(&myRing, &arr[0], 10, sizeof(uint8_t));
 
-   TEST_ASSERT_EQUAL(OK, RingWriteElement(&myRing, testValue));
+   TEST_ASSERT_EQUAL(OK, RingWriteElement(&myRing, &testValue));
    TEST_ASSERT_EQUAL(testValue, arr[0]);
 }
 
 TEST(ring_tests, write_to_null_buf)
 {
    uint8_t testValue = 10;
-   TEST_ASSERT_EQUAL(NO_PTR, RingWriteElement(NULL, testValue));
+   TEST_ASSERT_EQUAL(NO_PTR, RingWriteElement(NULL, &testValue));
 }
 
 TEST(ring_tests, write_multiple_bytes)
@@ -69,7 +69,7 @@ TEST(ring_tests, write_multiple_bytes)
    uint8_t arr[10];
    uint8_t sizeNum = 5;
    uint8_t testValues[5] = {1,2,3,4,5};
-   RingInit(&myRing, &arr[0], 10);
+   RingInit(&myRing, &arr[0], 10, sizeof(uint8_t));
 
    TEST_ASSERT_EQUAL(OK, RingWriteElements(&myRing, &testValues[0], sizeNum));
    TEST_ASSERT_EQUAL_UINT8_ARRAY(&testValues[0], &arr[0], 5);
@@ -80,7 +80,7 @@ TEST(ring_tests, write_multiple_bytes_from_null)
    RingBuffer_t myRing;
    uint8_t arr[10];
    uint8_t sizeNum = 5;
-   RingInit(&myRing, &arr[0], 10);
+   RingInit(&myRing, &arr[0], 10, sizeof(uint8_t));
 
    TEST_ASSERT_EQUAL(NO_PTR, RingWriteElements(&myRing, NULL, sizeNum));
 }
@@ -99,7 +99,7 @@ TEST(ring_tests, write_multiple_bytes_no_data)
    uint8_t arr[10];
    uint8_t sizeNum = 0;
    uint8_t testValues[5] = {1,2,3,4,5};
-   RingInit(&myRing, &arr[0], 10);
+   RingInit(&myRing, &arr[0], 10, sizeof(uint8_t));
 
    TEST_ASSERT_EQUAL(NO_DATA, RingWriteElements(&myRing, &testValues[0], sizeNum));
 }
@@ -111,7 +111,7 @@ TEST(ring_tests, write_multiple_bytes_overlap)
    uint8_t sizeNum = 5;
    uint8_t testValues[5] = {1,2,3,4,5};
    uint8_t arrRef[10] = {3,4,5,0,0,0,0,0,1,2};
-   RingInit(&myRing, &arr[0], 10);
+   RingInit(&myRing, &arr[0], 10, sizeof(uint8_t));
    // Sztuczne przesunięcie wskaźnika
    myRing.writePtr = 8;
    myRing.readPtr = 8;
@@ -126,7 +126,7 @@ TEST(ring_tests, write_multiple_bytes_no_place)
    uint8_t sizeNum = 6;
    uint8_t testValues[6] = {1,2,3,4,5,6};
    uint8_t arrRef[10] = {1,2,3,4,5,6,1,2,3,0};
-   RingInit(&myRing, &arr[0], 10);
+   RingInit(&myRing, &arr[0], 10, sizeof(uint8_t));
    TEST_ASSERT_EQUAL(OK, RingWriteElements(&myRing, &testValues[0], sizeNum));
    TEST_ASSERT_EQUAL(NO_PLACE, RingWriteElements(&myRing, &testValues[0], sizeNum));
    TEST_ASSERT_EQUAL(NO_PLACE, RingWriteElements(&myRing, &testValues[0], sizeNum - 1));
@@ -142,8 +142,8 @@ TEST(ring_tests, read_byte_after_init)
    uint8_t arr[10];
    uint8_t data;
    
-   RingInit(&myRing, &arr[0], 10);
-   TEST_ASSERT_EQUAL(NO_DATA, RingReadByte(&myRing, &data));
+   RingInit(&myRing, &arr[0], 10, sizeof(uint8_t));
+   TEST_ASSERT_EQUAL(NO_DATA, RingReadElement(&myRing, &data));
 }
 
 TEST(ring_tests, read_bytes_to_empty)
@@ -153,15 +153,15 @@ TEST(ring_tests, read_bytes_to_empty)
    uint8_t sizeNum = 6;
    uint8_t testValues[6] = {1,2,3,4,5,6};
    uint8_t data;
-   RingInit(&myRing, &arr[0], 10);
-   TEST_ASSERT_EQUAL(NO_DATA, RingReadByte(&myRing, &data));
+   RingInit(&myRing, &arr[0], 10, sizeof(uint8_t));
+   TEST_ASSERT_EQUAL(NO_DATA, RingReadElement(&myRing, &data));
 
    TEST_ASSERT_EQUAL(OK, RingWriteElements(&myRing, &testValues[0], sizeNum));
    for(uint8_t i = 0; i < sizeNum; i++){
-      TEST_ASSERT_EQUAL(OK, RingReadByte(&myRing, &data));
+      TEST_ASSERT_EQUAL(OK, RingReadElement(&myRing, &data));
       TEST_ASSERT_EQUAL(testValues[i], data);
    }
-   TEST_ASSERT_EQUAL(NO_DATA, RingReadByte(&myRing, &data));
+   TEST_ASSERT_EQUAL(NO_DATA, RingReadElement(&myRing, &data));
    //TEST_FAIL_MESSAGE("initial test setup");
 }
 
@@ -170,7 +170,7 @@ TEST(ring_tests, get_size)
    RingBuffer_t myRing;
    uint8_t arr[10];
    
-   RingInit(&myRing, &arr[0], 10);
+   RingInit(&myRing, &arr[0], 10, sizeof(uint8_t));
    TEST_ASSERT_EQUAL(10, RingGetElementsCapacity(&myRing));
 }
 
@@ -180,7 +180,7 @@ TEST(ring_tests, get_space)
    uint8_t arr[10];
    uint8_t data[5] = {1, 1, 1, 1, 1};
    
-   RingInit(&myRing, &arr[0], 10);
+   RingInit(&myRing, &arr[0], 10, sizeof(uint8_t));
    RingWriteElements(&myRing, &data[0], 5);
    TEST_ASSERT_EQUAL(4, RingGetSpace(&myRing));
 }
