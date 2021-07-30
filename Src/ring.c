df85e658f1c0e7cdc9c550fcb398b4b772e9438e
Kacper Brzostowski <kacper.brzostowski@lpnplant.io>
2021-07-30 11:37:50 +0200

    Fromatting fixes

--- Src/ring.c
+++ Src/ring.c
@@ -17,32 +17,33 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
-#include "ring.h"
+#include "Src/ring.h"
 
 /**< Modulo for operations on array indexes. */
 #define MODULO_BUF(value, max) ((value) % (max))
 
-uint32_t RingGetElementsCapacity (RingBuffer_t* buffer) {
+uint32_t RingGetElementsCapacity(RingBuffer_t* buffer) {
     return buffer -> size;
 }
 
-uint32_t RingGetSpace (RingBuffer_t* buffer) {
+uint32_t RingGetSpace(RingBuffer_t* buffer) {
     return buffer -> place;
 }
 
-uint32_t RingGetDataCnt (RingBuffer_t* buffer) {
+uint32_t RingGetDataCnt(RingBuffer_t* buffer) {
     return buffer -> size - RingGetSpace(buffer);
 }
 
 /* DONE: Add null pointer exceptions. */
-RingStatus_t RingInit (RingBuffer_t* buffer, void* arrayBuffer, size_t bufferSize, size_t elementSize){
-    if(NULL == buffer) return NO_PTR;
-    if(NULL == arrayBuffer) return NO_PTR;
+RingStatus_t RingInit(RingBuffer_t* buffer, void* arrayBuffer,
+        size_t bufferSize, size_t elementSize) {
+    if (NULL == buffer) return NO_PTR;
+    if (NULL == arrayBuffer) return NO_PTR;
 
     memset(buffer, 0, sizeof(RingBuffer_t));
 
-    if(arrayBuffer == NULL) return NO_PTR;
-    if(bufferSize <= 0) return NO_DATA;
+    if (arrayBuffer == NULL) return NO_PTR;
+    if (bufferSize <= 0) return NO_DATA;
 
     buffer -> buffer = arrayBuffer;
     buffer -> size = bufferSize;
@@ -56,25 +57,26 @@ RingStatus_t RingInit (RingBuffer_t* buffer, void* arrayBuffer, size_t bufferSiz
     return OK;
 }
 
-RingStatus_t RingInitAlloc (RingBuffer_t* buffer, size_t bufferSize, size_t elementSize){
+RingStatus_t RingInitAlloc(RingBuffer_t* buffer, size_t bufferSize,
+        size_t elementSize) {
     void* ptr;
     size_t reqSize = elementSize * bufferSize;
-    if(NULL == buffer){
+    if (NULL == buffer) {
         return NO_PTR;
     }
     ptr = malloc(reqSize);
-    if(NULL == ptr){
+    if (NULL == ptr) {
         return NO_PTR;
     }
     return RingInit(buffer, ptr, bufferSize, elementSize);
 }
 
-RingStatus_t RingWriteElement (RingBuffer_t* buffer, void* data){
+RingStatus_t RingWriteElement(RingBuffer_t* buffer, void* data) {
     RingStatus_t retval = OK;
 
-    if(buffer == NULL) return NO_PTR;
-    if(data == NULL) return NO_PTR;
-    if(buffer -> buffer == NULL) return NO_PTR;
+    if (buffer == NULL) return NO_PTR;
+    if (data == NULL) return NO_PTR;
+    if (buffer -> buffer == NULL) return NO_PTR;
 
     uint32_t tempHead = buffer -> writePtr;
     uint32_t tempTail = buffer -> readPtr;
@@ -85,23 +87,23 @@ RingStatus_t RingWriteElement (RingBuffer_t* buffer, void* data){
     wrPtr = buffer -> buffer + tempHead;
 
     tempHead = MODULO_BUF(tempHead + elSize, bufferSize);
-    if(tempHead != tempTail){
+    if (tempHead != tempTail) {
         memcpy(wrPtr, data, elSize);
         buffer -> writePtr = tempHead;
-        buffer -> place --;
-    }else{
+        buffer -> place--;
+    } else {
         retval = NO_PLACE;
     }
     return retval;
 }
 
-RingStatus_t RingWriteElements (RingBuffer_t* buffer, void* data, size_t len){
+RingStatus_t RingWriteElements(RingBuffer_t* buffer, void* data, size_t len) {
     RingStatus_t retval = OK;
 
-    if(buffer == NULL) return NO_PTR;
-    if(data == NULL) return NO_PTR;
-    if(buffer -> buffer == NULL) return NO_PTR;
-    if(len <= 0) return NO_DATA;
+    if (buffer == NULL) return NO_PTR;
+    if (data == NULL) return NO_PTR;
+    if (buffer -> buffer == NULL) return NO_PTR;
+    if (len <= 0) return NO_DATA;
 
     uint32_t tempHead = buffer -> writePtr;
     uint32_t tempPlace = buffer -> place;
@@ -114,8 +116,8 @@ RingStatus_t RingWriteElements (RingBuffer_t* buffer, void* data, size_t len){
 
     wrPtr = buffer -> buffer + tempHead;
 
-    if(tempPlace >= len){
-        if(tempHead + bytesToWrite >= bufferSizeB){
+    if (tempPlace >= len) {
+        if (tempHead + bytesToWrite >= bufferSizeB) {
             uint32_t temp1, temp2;
             temp1 = bufferSizeB - tempHead;
             memcpy(wrPtr, data, temp1);
@@ -123,21 +125,21 @@ RingStatus_t RingWriteElements (RingBuffer_t* buffer, void* data, size_t len){
             temp2 = len - temp1;
             wrPtr = buffer -> buffer;
             memcpy(wrPtr, data, temp2);
-        }else{
+        } else {
             memcpy(wrPtr, data, len);
             tempHead = MODULO_BUF(tempHead + len, bufferSize);
         }
         tempPlace -= len;
         buffer -> writePtr = tempHead;
         buffer -> place = tempPlace;
-    }else{
+    } else {
         retval = NO_PLACE;
     }
 
     return retval;
 }
 
-RingStatus_t RingReadElement (RingBuffer_t* buffer, void* data){
+RingStatus_t RingReadElement(RingBuffer_t* buffer, void* data) {
     RingStatus_t retval = OK;
     uint32_t tempHead = buffer -> writePtr;
     uint32_t tempTail = buffer -> readPtr;
@@ -147,27 +149,27 @@ RingStatus_t RingReadElement (RingBuffer_t* buffer, void* data){
     void* wrPtr;
     wrPtr = buffer -> buffer + tempTail;
 
-    if(tempHead != tempTail){
+    if (tempHead != tempTail) {
         memcpy(data, wrPtr, elSize);
         tempTail = MODULO_BUF(tempTail + 1, bufferSize);
         tempPlace++;
         buffer -> readPtr = tempTail;
         buffer -> place = tempPlace;
-    }else{
+    } else {
         retval = NO_DATA;
     }
     return retval;
 }
 
-RingStatus_t RingReadElements (RingBuffer_t* buffer, void* data, size_t len){
+RingStatus_t RingReadElements(RingBuffer_t* buffer, void* data, size_t len) {
     RingStatus_t retval = OK;
     uint32_t tempHead = buffer -> writePtr;
     uint32_t tempTail = buffer -> readPtr;
     uint32_t tempPlace = buffer -> place;
     uint8_t* wrPtr;
     wrPtr = buffer -> buffer + tempHead;
-    if(tempHead != tempTail){
-        if(tempTail + len > buffer -> size - 1){
+    if (tempHead != tempTail) {
+        if (tempTail + len > buffer -> size - 1) {
             uint32_t temp1, temp2;
             temp1 = buffer -> size - tempTail;
             memcpy(data, wrPtr, temp1);
@@ -176,32 +178,33 @@ RingStatus_t RingReadElements (RingBuffer_t* buffer, void* data, size_t len){
             wrPtr = buffer -> buffer;
             memcpy(data, wrPtr, temp2);
             tempTail = temp2;
-        }else{
+        } else {
             memcpy(data, wrPtr, len);
             tempTail += len;
         }
         tempPlace += len;
         buffer -> readPtr = tempTail;
         buffer -> place = tempPlace;
-    }else{
+    } else {
         retval = NO_DATA;
     }
     return retval;
 }
 
-uint32_t RingGetHead (RingBuffer_t* buffer){
+uint32_t RingGetHead(RingBuffer_t* buffer) {
     return buffer -> writePtr;
 }
 
-uint32_t RingGetTail (RingBuffer_t* buffer){
+uint32_t RingGetTail(RingBuffer_t* buffer) {
     return buffer -> readPtr;
 }
 
-RingStatus_t RingGetLastElement(RingBuffer_t* buffer, void* element){
+RingStatus_t RingGetLastElement(RingBuffer_t* buffer, void* element) {
     RingStatus_t ret = OK;
-    if(buffer && element){
-        memcpy(element, buffer -> buffer + buffer -> writePtr, buffer -> elementSize);
-    }else{
+    if (buffer && element) {
+        memcpy(element, buffer -> buffer + buffer -> writePtr,
+            buffer -> elementSize);
+    } else {
         ret = NO_PTR;
     }
     return ret;
--- Src/ring.h
+++ Src/ring.h
@@ -24,8 +24,8 @@
  *
  */
 
-#ifndef RING_H_
-#define RING_H_
+#ifndef SRC_RING_H_
+#define SRC_RING_H_
 
 #include <stdint.h>
 
@@ -39,26 +39,26 @@
  * @brief Ring buffer status enumerator.
  *
  */
-typedef enum{
-	NO_PTR = -3, /**< Returned if null pointer was given as parameter. */
-	NO_DATA = -2, /**< Returned if there was no data to read in buffer. */
-	NO_PLACE = -1, /**< Returned if there was no place to write data in buffer. */
-	OK = 1 /**< Returned when write/read was succesfull/ */
+typedef enum {
+    NO_PTR = -3, /**< Returned if null pointer was given as parameter. */
+    NO_DATA = -2, /**< Returned if there was no data to read in buffer. */
+    NO_PLACE = -1, /**< Returned if ther's no place to write data in buffer. */
+    OK = 1 /**< Returned when write/read was succesfull. */
 } RingStatus_t;
 
 /**
  * @brief Buffer handler structure.
  *
  */
-typedef struct{
-	size_t size; /**< Size of buffer given in elements. */
-	size_t elementSize; /**< Size of one buffer element. */
-	size_t sizeB; /**< Size of buffer given in bytes. */
-	size_t elementsInBuffer; /**< Current count of elements in buffer. */
-	uint32_t writePtr; /**< Buffer next write pointer. */
-	uint32_t readPtr; /**< Buffer next read pointer. */
-	uint32_t place; /**< Place available in buffer. */
-	void* buffer; /**< Pointer to array holding ring buffer. */
+typedef struct {
+    size_t size; /**< Size of buffer given in elements. */
+    size_t elementSize; /**< Size of one buffer element. */
+    size_t sizeB; /**< Size of buffer given in bytes. */
+    size_t elementsInBuffer; /**< Current count of elements in buffer. */
+    uint32_t writePtr; /**< Buffer next write pointer. */
+    uint32_t readPtr; /**< Buffer next read pointer. */
+    uint32_t place; /**< Place available in buffer. */
+    void* buffer; /**< Pointer to array holding ring buffer. */
 } RingBuffer_t;
 
 /**
@@ -67,7 +67,7 @@ typedef struct{
  * @param buffer 	Pointer to buffer structure, which size has to be returned.
  * @retval 			Size of pointed ring buffer.
  */
-uint32_t RingGetElementsCapacity (RingBuffer_t* buffer);
+uint32_t RingGetElementsCapacity(RingBuffer_t* buffer);
 
 /**
  * @brief Function that returns available space in selected buffer.
@@ -75,7 +75,7 @@ uint32_t RingGetElementsCapacity (RingBuffer_t* buffer);
  * @param buffer Pointer to buffer structure, which size has to be returned.
  * @return uint32_t Available size in provided buffer.
  */
-uint32_t RingGetSpace (RingBuffer_t* buffer);
+uint32_t RingGetSpace(RingBuffer_t* buffer);
 
 /**
  * @brief Function that returns count of data available in buffer.
@@ -83,7 +83,7 @@ uint32_t RingGetSpace (RingBuffer_t* buffer);
  * @param buffer Pointer to buffer structure
  * @return uint32_t Data count in buffer.
  */
-uint32_t RingGetDataCnt (RingBuffer_t* buffer);
+uint32_t RingGetDataCnt(RingBuffer_t* buffer);
 
 /**
  * @brief Function to initialize selected ring buffer.
@@ -94,7 +94,8 @@ uint32_t RingGetDataCnt (RingBuffer_t* buffer);
  * @param elementSize Size of one element
  * @return RingStatus_t
  */
-RingStatus_t RingInit (RingBuffer_t* buffer, void* arrayBuffer, size_t bufferSize, size_t elementSize);
+RingStatus_t RingInit(RingBuffer_t* buffer, void* arrayBuffer,
+        size_t bufferSize, size_t elementSize);
 
 /**
  * @brief Function to initialize ring buffer with memory allocation
@@ -104,7 +105,8 @@ RingStatus_t RingInit (RingBuffer_t* buffer, void* arrayBuffer, size_t bufferSiz
  * @param elementSize Size of one element
  * @return RingStatus_t
  */
-RingStatus_t RingInitAlloc (RingBuffer_t* buffer, size_t bufferSize, size_t elementSize);
+RingStatus_t RingInitAlloc(RingBuffer_t* buffer, size_t bufferSize,
+        size_t elementSize);
 
 /**
  * @brief Addes one byte to the end of buffer.
@@ -113,7 +115,7 @@ RingStatus_t RingInitAlloc (RingBuffer_t* buffer, size_t bufferSize, size_t elem
  * @param data Data to write.
  * @return RingStatus_t Status of write process.
  */
-RingStatus_t RingWriteElement (RingBuffer_t* buffer, void* data);
+RingStatus_t RingWriteElement(RingBuffer_t* buffer, void* data);
 
 /**
  * @brief Writes multiple bytes to buffer.
@@ -123,7 +125,7 @@ RingStatus_t RingWriteElement (RingBuffer_t* buffer, void* data);
  * @param len Length of data.
  * @return RingStatus_t Write status.
  */
-RingStatus_t RingWriteElements (RingBuffer_t* buffer, void* data, size_t len);
+RingStatus_t RingWriteElements(RingBuffer_t* buffer, void* data, size_t len);
 
 /**
  * @brief Reads one byte from buffer.
@@ -132,7 +134,7 @@ RingStatus_t RingWriteElements (RingBuffer_t* buffer, void* data, size_t len);
  * @param data Pointer to save data.
  * @return RingStatus_t Read status.
  */
-RingStatus_t RingReadElement (RingBuffer_t* buffer, void* data);
+RingStatus_t RingReadElement(RingBuffer_t* buffer, void* data);
 
 /**
  * @brief Reads multiple bytes from buffer.
@@ -142,7 +144,7 @@ RingStatus_t RingReadElement (RingBuffer_t* buffer, void* data);
  * @param len Length of data to read.
  * @return RingStatus_t Read status.
  */
-RingStatus_t RingReadElements (RingBuffer_t* buffer, void* data, size_t len);
+RingStatus_t RingReadElements(RingBuffer_t* buffer, void* data, size_t len);
 
 /**
  * @brief Returns write pointer of buffer.
@@ -150,7 +152,7 @@ RingStatus_t RingReadElements (RingBuffer_t* buffer, void* data, size_t len);
  * @param buffer Buffer to get pointer.
  * @return uint32_t Write pointer.
  */
-uint32_t RingGetHead (RingBuffer_t* buffer);
+uint32_t RingGetHead(RingBuffer_t* buffer);
 
 /**
  * @brief Returns read pointer of buffer.
@@ -158,7 +160,7 @@ uint32_t RingGetHead (RingBuffer_t* buffer);
  * @param buffer Buffer to get pointer.
  * @return uint32_t Read pointer.
  */
-uint32_t RingGetTail (RingBuffer_t* buffer);
+uint32_t RingGetTail(RingBuffer_t* buffer);
 
 /**
  * @brief Gets last element from buffer without taking it from buffer.
@@ -172,4 +174,4 @@ RingStatus_t RingGetLastElement(RingBuffer_t* buffer, void* element);
  * @}
  *
  */
-#endif /* RING_H_ */
+#endif /* SRC_RING_H_ */
